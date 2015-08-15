f04f11cdd7ff0a26e4e634ebe80c78726c4c7836
Yong Chen <cysnap@gmail.com>
2015-08-15 11:31:15 +0800

    found solutions on stackover

--- functions.php
+++ functions.php
@@ -177,19 +177,11 @@ function register_my_menus() {
 add_action( 'init', 'register_my_menus' );
 
 //remove header menu's classes and ids
-add_filter('nav_menu_css_class', 'special_nav_class', 10, 2);
+add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);
 add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
 add_filter('page_css_class', 'my_css_attributes_filter', 100, 1);
 function my_css_attributes_filter($var) {
-  return is_array($var) ? array() : '';
+	return is_array($var) ? array_intersect($var, array('current-menu-item')) : '';
 }
-function special_nav_class($classes, $item){
-     if( in_array('current-menu-item', $classes) ){
-	             $classes[] = 'active ';
-	      }
-	      return $classes;
-	 }
-
-
 ?>
 
