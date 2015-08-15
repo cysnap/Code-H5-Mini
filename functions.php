88efb7c45e4c05f28cd0cf3355e01073bab3837e
Yong Chen <cysnap@gmail.com>
2015-08-15 11:06:13 +0800

    too many menu classess,less

--- functions.php
+++ functions.php
@@ -178,11 +178,11 @@ add_action( 'init', 'register_my_menus' );
 
 //remove header menu's classes and ids
 //add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);
-//add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
-//add_filter('page_css_class', 'my_css_attributes_filter', 100, 1);
-//function my_css_attributes_filter($var) {
-//  return is_array($var) ? array() : '';
-//}
+add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
+add_filter('page_css_class', 'my_css_attributes_filter', 100, 1);
+function my_css_attributes_filter($var) {
+  return is_array($var) ? array() : '';
+}
 
 
 
