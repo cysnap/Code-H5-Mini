57737f945e730f4438981ecbf0886f00f1244560
Yong Chen <cysnap@gmail.com>
2015-08-15 19:36:32 +0800

    404 page now in HTML5

--- 404.php
+++ 404.php
@@ -1,5 +1,6 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head profile="http://gmpg.org/xfn/11">
+<!DOCTYPE HTML>
+<html lan="zh-CN">
+<head>
 <title>404 | <?php bloginfo('name'); ?></title>
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.css" type="text/css" media="screen" />
 </head>
@@ -10,7 +11,7 @@
 			<a href="<?php echo get_settings('home'); ?>/"><h1>测试版资讯::IT业界资讯及时推送！</h1></a>
 		</div>
 		<div class="kudos-widget">
-			<div class="kudos-title"><p>404！这个页面被缺心眼的站长给弄丢！再也找不回来了！</p></div>
+			<div class="kudos-title"><p>404！这个页面被站长的虚拟喵给吃了！再也找不回来了！</p></div>
 			<div class="keywords clearfix"><h3>站内热门话题</h3><?php wp_tag_cloud('smallest=9&largest=15&number=8&orderby=count&order=DESC'); ?></div>
 			<div class="keywords">
 				<h3>最新文章</h3>
