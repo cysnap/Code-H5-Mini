b9e7686b3eea74c97b08f7104ee1e3c74e6318da
Yong Chen <cysnap@gmail.com>
2015-08-17 14:28:00 +0800

    changed a way to fix

--- js/functions.js
+++ js/functions.js
@@ -445,30 +445,17 @@ function appendReply(insertStr, commentBox) {
 	field.focus();
 }
 
-// fixed menu function
-// Create a clone of the menu, right next to original.
-$('.nav').addClass('original').clone().insertAfter('.nav').addClass('cloned').css('position', 'fixed').css('top', '0').css('margin-top', '0').css('z-index', '500').removeClass('original').hide();
+//<![CDATA[
+$(window).load(function(){
+/* Dynamic top menu positioning*/
+var num = 50; //number of pixels before modifying styles
 
-scrollIntervalID = setInterval(stickIt, 10);
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.nav').addClass('fixed');
+    } else {
+        $('.nav').removeClass('fixed');
+    }
+});
+});//]]> 
 
-function stickIt() {
-
-  var orgElementPos = $('.original').offset();
-  orgElementTop = orgElementPos.top;
-
-  if ($(window).scrollTop() >= (orgElementTop)) {
-    // scrolled past the original position; now only show the cloned, sticky element.
-
-    // Cloned element should always have same left position and width as original element.     
-    orgElement = $('.original');
-    coordsOrgElement = orgElement.offset();
-    leftOrgElement = coordsOrgElement.left;
-    widthOrgElement = orgElement.css('width');
-    $('.cloned').css('left', leftOrgElement + 'px').css('top', 0).css('width', widthOrgElement).show();
-    $('.original').css('visibility', 'hidden');
-  } else {
-    // not scrolled past the menu; only show the original menu.
-    $('.cloned').hide();
-    $('.original').css('visibility', 'visible');
-  }
-}
