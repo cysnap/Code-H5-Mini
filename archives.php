a722efb41a58fe90df6bb1c0c580b70ea535b877
Yong Chen <cysnap@gmail.com>
2015-08-17 12:00:34 +0800

    not archives page looks better

--- archives.php
+++ archives.php
@@ -22,7 +22,7 @@ Template Name: All Topics
 <div class="sidebar">
 	<ul>
 	
-		<li class="sidebar-li"><div class="mobile"></div></li>
+		<li><div class="mobile"></div></li>
 		
 		<?php if (!is_search()) {$search_text = "站内新闻搜索";} else {$search_text = "$s";} ?>
 		<li class="sidebar-li">
@@ -34,28 +34,7 @@ Template Name: All Topics
 		</div>
 		</li>
 		
-		<li class="sidebar-li">
-			<h2 class="sidebar-title">存档</h2>
-			<div class="notice list_archives">
-			<form action="<?php bloginfo('url'); ?>/" method="get">
-				<?php
-				$select = wp_dropdown_categories('show_option_none=选择分类&show_count=1&orderby=name&echo=0');
-				$select = preg_replace("#<select([^>]*)>#", "<select$1 onchange='return this.form.submit()'>", $select);
-				echo $select;
-				?>
-			<noscript><input  type="submit" value="View" /></noscript>
-			</form>
-
-			<select class="postform_r" onChange='document.location.href=this.options[this.selectedIndex].value;'>
-				<option><?php echo attribute_escape(__('Select Month')); ?></option>
-				<?php wp_get_archives('type=monthly&limit=12&format=option&show_post_count=1'); ?>
-			</select>
-			</div>
-		</li>
-
-		<li class="sidebar-li">
-			<?php show_ads(3); ?>
-		</li>
+		<li><?php dynamic_sidebar( '300x250-Ads' ); ?></li>
 
 	</ul>
 </div>
