040b9b5e4289a32eee6992acac8e83e7804bfc80
Yong Chen <cysnap@gmail.com>
2016-04-21 13:57:48 +0800

    little modify

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </main> 
 <footer>
 	<div class="footer-credits">
-	<span>&copy; Copyright 2009 - <?php echo $showtime=date("Y");?> <a href="<?php echo get_settings('home'); ?>"  target="_blank"><?php bloginfo('name'); ?></a>； <?php echo get_num_queries();?> Queries in <?php timer_stop(1);?> Seconds.</span>
+	<span>&copy; Copyright 2009 - <?php echo $showtime=date("Y");?> <a href="<?php echo get_settings('home'); ?>"><?php bloginfo('name'); ?></a>； <?php echo get_num_queries();?> Queries in <?php timer_stop(1);?> Seconds.</span>
 	<span>除特别声明，本站转载内容均不代表本站观点。请遵守相关法律法规，理性使用评论功能。</span>
 		<?php if(is_single() || is_page()): add_duoshuo_js_to_footer(); endif;?>
 	</div>
